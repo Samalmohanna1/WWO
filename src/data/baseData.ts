fb68cc2ada560169b52f5ab9e4e57d04d7f26506
Sam Almohanna <webdevsumayah@gmail.com>
2025-10-02 13:53:08 -0400

    feat: setup base layout & theme.

---
+++ src/data/baseData.ts
@@ -0,0 +1,8 @@
+export default {
+	title: `Weird Web October Challenge`,
+	description: `This site is part of the Weird Web October challenge, a creative daily web development event throughout October encouraging fun and spooky projects.`,
+	url: ``,
+	ogImage: '/og-image.png',
+	githubURL: `https://github.com/samalmohanna1`,
+	portfolioURL: `https://sam-almohanna.com`,
+}

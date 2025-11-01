575e0db0a4e7b5d71f087372705811ed835837e3
Sam Almohanna <webdevsumayah@gmail.com>
2025-11-01 15:50:10 -0400

    feat: day 31.

--- src/data/wwoData.ts
+++ src/data/wwoData.ts
@@ -30,6 +30,6 @@ export default {
         { id: 28, url: 'Tables', status: 'launched' },
         { id: 29, url: 'Surprise', status: 'launched' },
         { id: 30, url: 'Deprecated', status: 'launched' },
-        { id: 31, url: 'Spooky', status: 'coming soon' },
+        { id: 31, url: 'Spooky', status: 'launched' },
     ]
 }
